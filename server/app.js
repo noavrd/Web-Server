6c7cfa4a7b2c11d0617086394db4650a45dc852b
noavrd <noavrd@gmail.com>
2022-02-08 13:36:25 +0200

    Post request to add title

--- server/app.js
+++ server/app.js
@@ -1,8 +1,20 @@
 const express = require('express');
 const cors = require('cors');
+const Task = require('./models/task');
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.post('/add', async (req, res) => {
+  try {
+    const { title } = req.body;
+    const newTask = new Task({ title });
+    await newTask.save();
+    res.send('Title added successfully');
+  } catch (err) {
+    console.log(err);
+    res.status(500).send(err);
+  }
+});
 module.exports = app;
